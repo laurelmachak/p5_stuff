91eab290e89180f94f77434815aba29e7780c742
laurelmachak <laurelmachak@gmail.com>
2019-05-11 00:43:28 -0700

    first info box steps

--- periodic_table/index.js
+++ periodic_table/index.js
@@ -1,12 +1,17 @@
 
 let element_data;
 let periodic_table = [];
+let info_box;
 let x;
 let y;
 let symbol;
 let cell_size = 50;
 let category;
 let fill_color;
+let name;
+let text_box_y = cell_size * 12;
+let text_box_height = cell_size * 3;
+let displayed_text = "click on a element!";
 
 let blue = [131, 165, 152];
 let green = [184, 187, 38];
@@ -39,8 +44,7 @@ function setup(){
     textFont('Roboto Mono');
     textSize(cell_size/2);
     textAlign(CENTER,CENTER);
-    console.log(element_data.elements[0].xpos);
-    console.log(element_data.elements[118]);
+    
 
     for (let i=0; i<element_data.elements.length; i++){
 
@@ -48,6 +52,7 @@ function setup(){
         y = element_data.elements[i].ypos * cell_size;
         symbol = element_data.elements[i].symbol;
         category = element_data.elements[i].category;
+        name = element_data.elements[i].name;
 
         if(category in categories){
             fill_color = categories[category];
@@ -55,28 +60,34 @@ function setup(){
             fill_color = aqua;
         }
 
-        periodic_table.push(new Cell(x, y,symbol, cell_size, fill_color));
+        periodic_table.push(new Cell(x, y, symbol,name, cell_size, fill_color));
     }
 
-    console.log(periodic_table);
+    info_box = new Info("testing 123!! HELLO!", cell_size, text_box_y, cell_size * 18, text_box_height);
 }
 
 function draw(){
     background(black);
     for (let i=0; i<periodic_table.length; i++){
-
+        if (periodic_table[i].clicked()){
+            displayed_text = periodic_table[i].name;
+        }
         periodic_table[i].display();
     }
+    info_box.words = displayed_text;
+    info_box.display();
+    // rect(cell_size, text_box_y, cell_size * 18, text_box_height);
 
 }
 
 class Cell{
-    constructor(x,y,symbol,size, color){
+    constructor(x,y,symbol,name,size, color){
         this.x = x;
         this.y = y;
         this.size = size;
         this.color = color;
         this.symbol = symbol;
+        this.name = name;
     }
 
     display(){
@@ -85,6 +96,36 @@ class Cell{
         rect(this.x, this.y, this.size, this.size);
         fill(0);
         text(this.symbol, this.x + this.size/2, this.y + this.size/2);
+    }
+
+    clicked(){
+        if (mouseIsPressed){
+            if (mouseX > this.x && mouseX < this.x + this.size){
+                if (mouseY > this.y && mouseY < this.y + this.size){
+                    return true;
+                }
+            }
+        }
+    }
+}
+
+class Info{
+    constructor(words,x,y, x_size, y_size){
+        this.words = words;
+        this.x = x;
+        this.y = y;
+        this.x_size = x_size;
+        this.y_size = y_size;
+    }
+
+    display(){
+        stroke(189, 174, 147);
+        strokeWeight(3);
+        fill(235, 219, 178);
+        rect(this.x, this.y, this.x_size, this.y_size);
+        fill(66, 123, 88);
+        noStroke();
+        text(this.words, this.x + this.x_size/2, this.y + this.y_size/2);
     }
 }
 
